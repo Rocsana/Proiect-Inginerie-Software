5f00ddd315fc7cd09dfe3ce6ac0f489dace53bb5
Rocsana <rocsana.suteu@gmail.com>
2020-10-26 20:39:50 +0300

    Commit2

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.park/ParkingLot .
-call docker rm -f ParkingLot
-call docker run -d -p 9080:9080 -p 9443:9443 --name ParkingLot com.park/ParkingLot
